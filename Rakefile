e53e5c6c8f228739d81849ba42b3e6d3df114e31
Marti D <me@martidolce.com>
2021-03-15 19:17:46 -0400

    Add requirements for listings migration/seed

--- Rakefile
+++ Rakefile
@@ -1,11 +1,10 @@
-# frozen_string_literal: true
-
-ENV['SINATRA_ENV'] ||= 'development'
+ENV["SINATRA_ENV"] ||= "development"
 
 require_relative './config/environment'
+require 'sinatra/activerecord/rake'
 
 desc 'Load core directories'
-def reload!
+get "/" do
   load_all './config' if Dir.exists?('./config')
   load_all './app' if Dir.exists?('./app')
   load_all './lib' if Dir.exists?('./lib')
--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,30 @@
-ENV['SINATRA_ENV'] ||= "development"
+# frozen_string_literal: true
+
+ENV['SINATRA_ENV'] ||= 'development'
 
 require 'bundler/setup'
+require 'mysql2'
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ActiveRecord::Base.logger = Logger.new($stdout)
+
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    adapter: 'mysql2',
+    encoding: 'utf8',
+    database: 'dev_twenty45',
+    username: 'root',
+    password: 'Palo5non!'
+  )
+end
+
+configure :test do
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
 
 require './app/controllers/application_controller'
 require_all 'app'
